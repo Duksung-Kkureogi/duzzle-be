9cd9d4b4aa03a3b456ff5ca66a8c4af644bdf69f
user4920 <dltjgus@duksung.ac.kr>
2024-05-03 18:23:54 +0900

    [SB-29]fix: MailModule 의존성 문제 해결

--- src/module/email/email.module.ts
+++ src/module/email/email.module.ts
@@ -1,10 +1,14 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { MailgunModule } from 'nestjs-mailgun';
 import { MailService } from './email.service';
 import { MailController } from './email.controller';
 import { ConfigService } from '../config/config.service';
+import { EnvironmentModule } from '../config/config.module';
+
+@Global()
 @Module({
   imports: [
+    EnvironmentModule,
     MailgunModule.forRoot({
       username: ConfigService.getConfig().MAILGUN_USERNAME,
       key: ConfigService.getConfig().MAILGUN_KEY,
