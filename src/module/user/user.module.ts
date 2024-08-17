064215884bed0906a8efa473d5744c1769f15038
user4920 <dltjgus@duksung.ac.kr>
2024-08-17 22:10:57 +0900

    Fix: 의존성 주입

--- src/module/user/user.module.ts
+++ src/module/user/user.module.ts
@@ -5,9 +5,18 @@ import { UserController } from './user.controller';
 import { CloudStorageModule } from '../cloudStorage/cloudStorage.module';
 import { PuzzleModule } from '../puzzle/puzzle.module';
 import { ItemModule } from '../item/item.module';
+import { SeasonHistoryModule } from '../season-history/season-history.module';
+import { QuestModule } from '../quest/quest.module';
 
 @Module({
-  imports: [RepositoryModule, CloudStorageModule, PuzzleModule, ItemModule],
+  imports: [
+    RepositoryModule,
+    CloudStorageModule,
+    PuzzleModule,
+    ItemModule,
+    SeasonHistoryModule,
+    QuestModule,
+  ],
   controllers: [UserController],
   providers: [UserService],
   exports: [UserService],
