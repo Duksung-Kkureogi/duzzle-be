2fbfb96ea973f545d76c6bb110126b7ebeb6ff9c
user4920 <dltjgus@duksung.ac.kr>
2024-09-26 16:47:48 +0900

    Feat: 거래 등록 시, contractId, seasonZoneId 유효성 검사

--- src/module/nft-exchange/nft-exchange.service.ts
+++ src/module/nft-exchange/nft-exchange.service.ts
@@ -5,10 +5,19 @@ import { AvailableNftsToRequestRequest } from './dto/available-nfts-to-request.d
 import { AvailableNftDto } from './dto/available-nfts.dto';
 import { ReportProvider } from 'src/provider/report.provider';
 import { PostNftExchangeRequest } from './dto/nft-exchange.dto';
+import { NftRepositoryService } from '../repository/service/nft.repository.service';
+import { ContractKey } from '../repository/enum/contract.enum';
+import { NFTType } from './dto/nft-asset';
+import { ZoneRepositoryService } from '../repository/service/zone.repository.service';
+import { InvalidParamsError } from 'src/types/error/application-exceptions/400-bad-request';
 
 @Injectable()
 export class NftExchangeService {
   constructor(
+    private readonly zoneRepositoryService: ZoneRepositoryService,
+
+    private readonly nftRepositoryService: NftRepositoryService,
+
     private readonly nftExchangeRepositoryService: NftExchangeRepositoryService,
   ) {}
 
@@ -34,6 +43,29 @@ export class NftExchangeService {
     userId: number,
     params: PostNftExchangeRequest,
   ): Promise<void> {
+    const Nfts = [...params.offeredNfts, ...params.requestedNfts];
+    const seasonZoneIds = (
+      await this.zoneRepositoryService.getSeasonZones()
+    ).map((zone) => zone.id);
+
+    for (const nft of Nfts) {
+      if (nft.type === NFTType.Material) {
+        const contract = await this.nftRepositoryService.findContractById(
+          nft.contractId,
+        );
+        if (!contract || contract.key !== ContractKey.ITEM_MATERIAL) {
+          throw new InvalidParamsError();
+        }
+      } else if (
+        nft.type === NFTType.Blueprint ||
+        nft.type === NFTType.PuzzlePiece
+      ) {
+        if (!seasonZoneIds.includes(nft.seasonZoneId)) {
+          throw new InvalidParamsError();
+        }
+      }
+    }
+
     await this.nftExchangeRepositoryService.postNftExchange({
       offerorUserId: userId,
       ...params,
