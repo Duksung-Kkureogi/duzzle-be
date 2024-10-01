0cdda5710b5cc8b29feff7a3757de9cee8aa16ab
user4920 <dltjgus@duksung.ac.kr>
2024-10-01 19:12:14 +0900

    Feat: NFTAsset class-validator 추가

--- src/module/nft-exchange/dto/nft-asset.ts
+++ src/module/nft-exchange/dto/nft-asset.ts
@@ -1,4 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsEnum, IsInt } from 'class-validator';
 
 export enum NFTType {
   Material = 'material',
@@ -8,23 +9,29 @@ export enum NFTType {
 
 export class MaterialNFT {
   @ApiProperty({ enum: [NFTType.Material] })
+  @IsEnum(NFTType)
   readonly type = NFTType.Material;
 
   @ApiProperty()
+  @IsInt()
   contractId: number; // 컨트랙트 ID 유효성 검사 중요
 
   @ApiProperty()
+  @IsInt()
   quantity: number;
 }
 
 export class BlueprintOrPuzzleNFT {
   @ApiProperty({ enum: [NFTType.Blueprint, NFTType.PuzzlePiece] })
+  @IsEnum(NFTType)
   readonly type: NFTType.Blueprint | NFTType.PuzzlePiece;
 
   @ApiProperty()
+  @IsInt()
   seasonZoneId: number;
 
   @ApiProperty()
+  @IsInt()
   quantity: number;
 }
 
